cd1e9bf2f0decfc95487db0bbc02719fe7ef1304
ikrgaurav <kkr2810@gmail.com>
2024-10-21 20:09:54 +0530

    fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,7 +5,7 @@ import { ThemeProvider } from "next-themes";
 export default function App({ Component, pageProps }) {
   return <ThemeProvider attribute="class">
     <Layout>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </Layout>
   </ThemeProvider>
 }
